720d6fbf22d00a589b9b7aca8fb0eeb7b1970fba
Graham <gtompkins1993@gmail.com>
2022-08-31 12:49:34 -0400

    Import repos from data file

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import data from '../data/repos.json';
 
 import axios from 'axios';
 var util = require('util');
@@ -13,8 +14,13 @@ repos.get('/', async (_: Request, res: Response) => {
     const repos = await axios.get(
       'https://api.github.com/users/silverorange/repos'
     );
-    console.log(Object.keys(repos));
-    console.log(repos.data);
+    // console.log(Object.keys(repos));
+    // console.log(repos.data);
+
+    // I am going to combine the data from the json file and the data
+    // I fetched from Axios, remove the repos where fork is true
+    // and sort repos based on date
+
     res.status(200).json({ status: 200, repos: repos.data });
   } catch (err: any) {
     console.log(err.message);
