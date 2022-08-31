754bfc98355d44791971d101faef921abd080468
Graham <gtompkins1993@gmail.com>
2022-08-31 13:46:07 -0400

    Set up reducer in context

--- web/src/ReposContext.tsx
+++ web/src/ReposContext.tsx
@@ -9,13 +9,70 @@ type repoState = {
 type RepoContextType = {
   loading: () => void;
   retrieveData: (payload: Repo[]) => void;
-  filter: (payload: number) => void;
+  filter: (payload: string) => void;
   state: repoState;
 };
 
-const ReposContext = createContext<RepoContextType>({
+export const ReposContext = createContext<RepoContextType>({
   loading: () => {},
   retrieveData: () => {},
   filter: () => {},
   state: { repos: [], status: '' },
 });
+
+type childrenProps = {
+  children: JSX.Element;
+};
+
+//Right now I have payload set to any => I will try to define the payloads later
+type actions =
+  | { type: 'loading' }
+  | { type: 'retrieveData'; payload: any }
+  | { type: 'filter'; payload: any };
+
+const reducer = (state: repoState, action: actions) => {
+  switch (action.type) {
+    case 'loading':
+      return { ...state, status: 'loading' };
+    case 'retrieveData':
+      return { ...state, status: 'loading' };
+    case 'filter':
+      //will figure this out later
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export const RepoContextProvider: React.FC<childrenProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, { repos: [], status: '' });
+
+  const loading = useCallback(() => {
+    dispatch({ type: 'loading' });
+  }, [dispatch]);
+
+  const retrieveData = useCallback(
+    (repos: Repo[]) => {
+      dispatch({ type: 'retrieveData', payload: repos });
+    },
+    [dispatch]
+  );
+
+  const filter = useCallback(
+    (search: string) => {
+      dispatch({ type: 'retrieveData', payload: search });
+    },
+    [dispatch]
+  );
+
+  return (
+    <ReposContext.Provider
+      value={{
+        loading,
+        retrieveData,
+        filter,
+        state,
+      }}
+    ></ReposContext.Provider>
+  );
+};
