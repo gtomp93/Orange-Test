be73758ba3930e3b7d522e37ae741857a6c7bd38
Graham <gtompkins1993@gmail.com>
2022-08-31 12:27:04 -0400

    Set up axios, use axios to get repo data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,22 @@
 import { Router, Request, Response } from 'express';
 
+import axios from 'axios';
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+  try {
+    res.status(200);
+    const repos = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    console.log(repos);
+    console.log(typeof repos);
+    res.status(200).json({ status: 200 });
+  } catch (err: any) {
+    console.log(err.message);
+  }
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
 });
